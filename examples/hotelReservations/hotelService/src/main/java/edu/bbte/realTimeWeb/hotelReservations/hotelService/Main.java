e9501a5aea59addfe45bc2a39a7dfb77b0f8952f
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-05-23 13:00:50 +0300

    creating projects

---
+++ examples/hotelReservations/hotelService/src/main/java/edu/bbte/realTimeWeb/hotelReservations/hotelService/Main.java
@@ -0,0 +1,7 @@
+package edu.bbte.realTimeWeb.hotelReservations.hotelService;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
