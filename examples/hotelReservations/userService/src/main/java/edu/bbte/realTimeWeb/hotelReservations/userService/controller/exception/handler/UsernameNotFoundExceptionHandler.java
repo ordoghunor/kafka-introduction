74595d5e8ceeea712220e3891e6a88e0fe9c9eb1
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-05-24 15:30:11 +0300

    added logout routing

---
+++ examples/hotelReservations/userService/src/main/java/edu/bbte/realTimeWeb/hotelReservations/userService/controller/exception/handler/UsernameNotFoundExceptionHandler.java
@@ -0,0 +1,25 @@
+package edu.bbte.realTimeWeb.hotelReservations.userService.controller.exception.handler;
+
+import edu.bbte.realTimeWeb.hotelReservations.userService.controller.dto.outgoing.ErrorData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class UsernameNotFoundExceptionHandler {
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorData handleFailedConstraintException(UsernameNotFoundException exception) {
+        LOGGER.error(exception.getMessage());
+        return new ErrorData(exception.getMessage());
+    }
+}
